32d831e69de8fb68c35624c3e485fbac06f48f75
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2024-09-21 17:32:20 -0300

    Return

--- net-return-simulator/src/app/app.component.ts
+++ net-return-simulator/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -21,8 +23,38 @@ export class AppComponent {
     annualRentalFee: 0
   };
 
-  onSubmit() {
+  showResult = false;
+  resultMessage = '';
+
+  onSubmit(form: any) {
     console.log(this.formData);
-    alert(JSON.stringify(this.formData));
+
+    const fees = [0.3, 0.25, 0.2];
+
+    const MNIs = fees.map(fee => Math.round(
+      this.formData.monthlyRent * (1 - fee)
+    ));
+
+    const ROIs = MNIs.map(MNI => Math.round(
+      100 * 12 * MNI / this.formData.purchasePrice
+    ));
+
+    let resultMessage = '';
+    
+    for (let year = 1; year <= fees.length; year++) {
+      resultMessage += `
+        <b>YEAR ${year}</b>
+        &nbsp;&nbsp;
+    
+        MNI: ${MNIs[year - 1]}
+        &nbsp;&nbsp;
+        ROI: ${ROIs[year - 1]}%
+    
+        <br>
+      `;
+    }
+    
+    this.resultMessage = resultMessage;
+    this.showResult = true;
   }
 }
